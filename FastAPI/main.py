93512fe2e93ea833676383249985f635e4f912a6
Dominik Farlik <xfarlik@mendelu.cz>
2025-06-20 15:42:26 +0200

    added route add record

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import psycopg2
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -32,8 +33,32 @@ def get_records():
         conn.rollback()
         raise
 
+class Record(BaseModel):
+    substance_name: int
+    amount: int
+    location_name: int
+    year: int
 
+def add_record():
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT record_id, s.name AS látka, l.name AS lokace, year AS rok
+                FROM records r
+                JOIN substances s ON r.substance_id = s.substance_id
+                JOIN locations l ON r.location_id = l.location_id;
+            """)
 
+            columns = [desc[0] for desc in cursor.description]
+            rows = cursor.fetchall()
+
+            results = [dict(zip(columns, row)) for row in rows]
+
+            return results
+    except Exception as e:
+        print("Nepodařilo se přidat záznam:", e)
+        conn.rollback()
+        raise
 
 @app.get("/")
 async def root():
@@ -47,3 +72,12 @@ async def say_hello(name: str):
 @app.get("/records")
 async def records():
     return {"records": get_records()}
+
+@app.post("/add_record")
+async def add_record(record: Record):
+    return {
+        "substance": record.substance_name,
+        "amount": record.amount,
+        "location": record.location_name,
+        "year": record.year
+    }

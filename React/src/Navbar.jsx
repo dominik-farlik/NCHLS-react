78703682930c34d33cf1ade3b73158fb9991d0d4
Dominik Farlík <dominik@farlik.cz>
2025-09-11 15:23:47 +0200

    feat: redesign navbar with bootstrap

--- React/src/Navbar.jsx
+++ React/src/Navbar.jsx
@@ -2,48 +2,78 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
-        <ul className="nav nav-underline nav-fill">
-            <li className="nav-item">
-                <NavLink
-                    className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
-                    to="/"
-                >
-                    Home
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                    NCHLS
                 </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink
-                    className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
-                    to="/add-substance"
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
                 >
-                    Přidat látku
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink
-                    className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
-                    to="/add-record"
-                >
-                    Přidat záznam
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink
-                    className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
-                    to="/substances"
-                >
-                    Seznam látek
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink
-                    className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
-                    to="/records"
-                >
-                    Seznam záznamů
-                </NavLink>
-            </li>
-        </ul>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    "nav-link" + (isActive ? " active" : "")
+                                }
+                                to="/"
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    "nav-link" + (isActive ? " active" : "")
+                                }
+                                to="/add-substance"
+                            >
+                                Přidat látku
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    "nav-link" + (isActive ? " active" : "")
+                                }
+                                to="/add-record"
+                            >
+                                Přidat záznam
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    "nav-link" + (isActive ? " active" : "")
+                                }
+                                to="/substances"
+                            >
+                                Seznam látek
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    "nav-link" + (isActive ? " active" : "")
+                                }
+                                to="/records"
+                            >
+                                Seznam záznamů
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
